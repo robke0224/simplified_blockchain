e22105f8410951cf1df8c24eca63ff36352ce1a8
robke0224 <154459735+robke0224@users.noreply.github.com>
2024-10-15 22:40:43 +0300

    -

---
+++ mylib.h
@@ -0,0 +1,41 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+#define INT_BITS 64
+
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <sstream>
+#include <filesystem> 
+#include <vector>
+#include <string>
+#include <fstream>
+#include <string>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
+#include <ctime> 
+#include <chrono>
+#include <random>
+#include <iomanip>
+#include <unordered_set>
+#include <cstring>
+#include <algorithm>
+#include <thread>
+#include <stdexcept>
+#include <unordered_map>
+#include <stack>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cerr;
+using std::move;
+using std::to_string;
+using std::stringstream;
+
+
+#endif
